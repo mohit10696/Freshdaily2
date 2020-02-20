688b8fabc8ad9edc74acad555ca14af0fd110c65
mohit10696 <mohitsojitraa@gmail.com>
2020-02-21 00:22:08 +0530

    Bottom navigation drawer

--- app/src/main/java/com/example/freshdaily/ui/dashboard/mydashboard.java
+++ app/src/main/java/com/example/freshdaily/ui/dashboard/mydashboard.java
@@ -1,14 +1,13 @@
 package com.example.freshdaily.ui.dashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
@@ -16,18 +15,21 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.freshdaily.R;
+import com.example.freshdaily.searchActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.w3c.dom.Text;
 
 public class mydashboard extends Fragment {
     BottomNavigationView BottomNavigation;
     View root;
+    EditText search;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
        root  = inflater.inflate(R.layout.fragment_maindashboard, container, false);
         BottomNavigation = root.findViewById(R.id.navigation);
         BottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
-
-
+        openFragment(HomeFragment.newInstance("", ""));
 
         return root;
     }
