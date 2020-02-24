78ef5443a18955553a8aeb34f50a93e6b117abaa
Divyesh Nabhoya <divyeshnabhoya2@gmail.com>
2020-02-24 14:26:25 +0530

    need

--- app/src/main/java/com/example/freshdaily/ui/MyAccount/MyAccountFragment.java
+++ app/src/main/java/com/example/freshdaily/ui/MyAccount/MyAccountFragment.java
@@ -1,28 +1,48 @@
 package com.example.freshdaily.ui.MyAccount;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.freshdaily.R;
 
 public class MyAccountFragment extends Fragment {
+    Button btn;
 
     private MyAccountViewModel homeViewModel;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(MyAccountViewModel.class);
         View root = inflater.inflate(R.layout.fragment_myaccount, container, false);
+        btn = getView().findViewById(R.id.EDIT);
+
+        /*btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                public void onClick(View v) {
+                    Fragment fragment = new Edit();
+                    FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.fragment_edit, fragment);
+                    fragmentTransaction.addToBackStack(null);
+                    fragmentTransaction.commit();
+                }
+            }
+        });*/
+
+
         /*final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
--- app/src/main/java/com/example/freshdaily/ui/dashboard/ContactFragment.java
+++ app/src/main/java/com/example/freshdaily/ui/dashboard/ContactFragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
 import com.example.freshdaily.R;
+import com.zoho.salesiqembed.ZohoSalesIQ;
 
 public class ContactFragment extends Fragment {
     View view;
@@ -17,9 +18,16 @@ public class ContactFragment extends Fragment {
         return fragment;
     }
 
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Fragment newFragment = new ContactFragment();
+        //Fragment newFragment = ContactFragment.newInstance();
+
         view =  inflater.inflate(R.layout.fragment_contact, container, false);
         return view;
+        //ZohoSalesIQ.init(  , "45xrGyIS2DrHrSx8LPVYaefKyTIzb8ApgFTT6rbYnyJOhOhAkZQdt2uYKKrBFIAb_in", "VXYedrQX8Skkl8hBUChWx%2FThmma%2FSCn9zV4pxPsw3Vyl91l4IjM8fpwgsYjLNXW8BZS2ind7lJppFjtNO685sskC%2B7h%2Fw%2FZUuMUmoCX%2BuTAiUtoObo%2BxyK4o5sR%2BW23AuOYA1YNKpgCrqkBaCMosdxvJqLssWmb4vAoMmiENoA4%3D");
+
+
     }
 }
