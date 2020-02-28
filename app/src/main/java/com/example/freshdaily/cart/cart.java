738159598193c1d42a24deaa03e734977ddcf4c2
mohit10696 <mohitsojitraa@gmail.com>
2020-02-28 08:04:53 +0530

    mohit

--- app/src/main/java/com/example/freshdaily/cart/cart.java
+++ app/src/main/java/com/example/freshdaily/cart/cart.java
@@ -6,13 +6,16 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,6 +24,7 @@ import com.example.freshdaily.API.retrofit;
 import com.example.freshdaily.DashBord;
 import com.example.freshdaily.DbAdapter;
 import com.example.freshdaily.R;
+import com.example.freshdaily.subscribeActitivty;
 import com.example.freshdaily.ui.dashboard.ProductList.adpaterProduct;
 import com.razorpay.Checkout;
 import com.razorpay.PaymentResultListener;
@@ -28,7 +32,11 @@ import com.razorpay.PaymentResultListener;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import okhttp3.MediaType;
@@ -45,9 +53,9 @@ public class cart extends AppCompatActivity implements PaymentResultListener {
     SharedPreferences.Editor editor;
     RecyclerView recyclerView;
     DbAdapter db;
-    Button clear;
-    TextView address;
-    String pidList[];
+    Button clear,payment,update;
+    TextView address,update_date,sdate;
+    String pidList[],date;
     Activity activity;
     List<modelcart> modelcarts;
     int amount = 0;
@@ -59,10 +67,19 @@ public class cart extends AppCompatActivity implements PaymentResultListener {
         recyclerView = findViewById(R.id.recyclecart);
         address = findViewById(R.id.addresshome);
         clear = (Button) findViewById(R.id.clear_cart);
+        payment = (Button) findViewById(R.id.payment);
+        update = (Button) findViewById(R.id.upda);
+        update_date = (TextView) findViewById(R.id.dat1);
+        sdate = (TextView) findViewById(R.id.sdate);
         address.setText(sharedpreferences.getString("address","not found"));
         activity = cart.this;
         db = new DbAdapter(getApplicationContext());
         db.open();
+
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+        date = formatter.format(new Date());
+        update_date.setText(setDateFromat(date));
+        sdate.setText(setDateFromat(date));
 
         try {
             Cursor row = db.fetch();
@@ -88,11 +105,53 @@ public class cart extends AppCompatActivity implements PaymentResultListener {
 //        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.VERTICAL,false));
 //        recyclerView.setAdapter(adapter);
 
+        payment.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                amount =0;
+                for(int i=0;i<pidList.length-1;i++)
+                    amount += Integer.parseInt(modelcarts.get(i).getCount())*Integer.parseInt(modelcarts.get(i).getPrice());
+                //Toast.makeText(getApplicationContext(),String.valueOf(amount),Toast.LENGTH_LONG).show();
+                amount = amount *100;
+                startPayment();
+            }
+        });
+
         clear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 db.deleteAll();
                 startActivity(new Intent(cart.this, DashBord.class));
+            }
+        });
+
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                                    // calender class's instance and get current date , month and year from calender
+                    final Calendar c = Calendar.getInstance();
+                    int mYear = c.get(Calendar.YEAR); // current year
+                    int mMonth = c.get(Calendar.MONTH); // current month
+                    int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+                    // date picker dialog
+                    final DatePickerDialog datePickerDialog = new DatePickerDialog(cart.this,new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                            // set day of month , month and year value in the edit text
+
+                            if ((monthOfYear + 1) < 10)
+                                date= dayOfMonth + "-0" + (monthOfYear + 1) + "-" + year;
+                            else
+                                date= dayOfMonth + "-" + (monthOfYear + 1) + "-" + year;
+                            update_date.setText(setDateFromat(date));
+                            sdate.setText(setDateFromat(date));
+
+                        }
+                    }, mYear, mMonth, mDay);
+                    datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis() + 1000 * 24 * 60 * 60);
+                    datePickerDialog.show();
+
             }
         });
 
@@ -113,7 +172,7 @@ public class cart extends AppCompatActivity implements PaymentResultListener {
                                 myResponse.getString("product_id"),
                                 myResponse.getString("product_name"),
                                 myResponse.getString("company_name"),
-                                "0",
+                                "1",
                                 myResponse.getString("quantity"),
                                 myResponse.getString("price"),
                                 myResponse.getString("photo")));
@@ -141,7 +200,17 @@ public class cart extends AppCompatActivity implements PaymentResultListener {
         recyclerView.setAdapter(adapter);
     }
 
-
+    String setDateFromat(String temp)
+    {
+        String s1 = new String();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+        try {
+            Date date = formatter.parse(temp);
+            formatter = new SimpleDateFormat("E, dd MMM yyyy");
+            s1= formatter.format(date);
+        } catch (ParseException e) {e.printStackTrace();}
+        return s1;
+    }
 
     @Override
     public void onPaymentSuccess(String s) {
@@ -197,7 +266,7 @@ public class cart extends AppCompatActivity implements PaymentResultListener {
              * Amount is always passed in currency subunits
              * Eg: "500" = INR 5.00
              */
-            options.put("amount", "500");
+            options.put("amount", String.valueOf(amount));
 
             checkout.open(activity, options);
         } catch(Exception e) {
